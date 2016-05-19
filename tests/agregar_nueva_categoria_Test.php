464ea6e7588ebda947283d36086f00924f55b8d1
Eleazar Gaitán <elotgamu@gmail.com>
2016-05-18 18:44:54 -0600

    Test  de entrar al contenido, nada aún

--- tests/agregar_nueva_categoria_Test.php
+++ tests/agregar_nueva_categoria_Test.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Database\Seeder;
 
+
 class agregar_nueva_categoria_Test extends TestCase
 {
     /*public function setUp()
@@ -24,23 +25,46 @@ class agregar_nueva_categoria_Test extends TestCase
      */
     public function test_add_categoria()
     {
-        //Route::enableFilters();
-        $credenciales = [
-           'name' => 'elver.galarga',
-           'password' => 'Elver.Galarga',
-       ];
+        $this->flushSession();
+        $this->withoutMiddleware();
+
+        $credenciales = array(
+            'name' => 'elver.galarga',
+            'password' => 'Elver.Galarga',
+        );
+
         Auth::shouldReceive('attempt')
              ->once()
              ->with($credenciales)
              ->andReturn(true);
-        //$this->withoutMiddleware();
-        $this->call('POST', '/login', $credenciales);
-        //$this->assertFalse($response->isOk());
+
+        $response = $this->call('POST', '/login', $credenciales);
+        Validator::shouldReceive('make')
+                 ->once()
+                 ->andReturn(Mockery::mock(['fails' => 'true']));
+        //$this->assertEquals(500, $response->getStatusCode());
+        //$crawler = $this->followRedirects();
+        //$this->assertInstanceOf('Illuminate\Http\RedirectResponse', $response);
+        $this->assertEquals($this->baseUrl . '/mi_contenido', $response->getTargetUrl());
+
+        //$this->assertResponseStatus(302);
+
+        //$this->assertTrue($response->isRedirection());
+
+        //$this->assertRedirectedTo('/mi_contenido');
+
+        //$crawler = $this->followRedirect();
+        //$this->assertRedirectedTo('/mi_contenido');
+
         //App::instance('Illuminate\Auth\Manager', $this->getAuthMock(true));
-        //$this->assertRedirectedToRoute('/mi_contenido', [] ,['flash']);
-        $this->assertRedirectedToAction('contenidosController@create');
+        //$this->assertRedirectedToRoute('mi_contenido');
+        //$this->assertRedirectedToAction('contenidosController@create');
         //Auth::shouldReceive('user')->andReturn($user = m::mock('StdClass'));
-        /*$this->visit('/mi_contenido')
+
+        /*$faker = Faker\Factory::create();
+        $user = new User(['name' => 'elver.galarga']);
+        $this->be($user);
+        $this->visit('/mi_contenido')
              ->press('Agregar categoría nueva')
              ->type($faker->word, 'txtcategoria')
              ->type($faker->text, 'txtdescripcion')
