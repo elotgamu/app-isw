8f635cd9c63f6e0a47c04ccb7ed6e4332664bf67
Eleazar Gaitán <elotgamu@gmail.com>
2016-06-13 13:07:27 -0600

    Enlace a cambio de contraseña olvidada

--- resources/views/formularios/login.blade.php
+++ resources/views/formularios/login.blade.php
@@ -1,56 +1,76 @@
 @extends ('layouts.default')
-@section ('title') Iniciar Sesi�n
-@stop
+@section ('title') Inicio de Sesión @stop
 
 @section ('content')
 <div class="landing-page-container">
 <div class="col-md-5 center-block quitar espacio3 text-center margen">
-       {!! Form::open(['url'=>'login']) !!}
-       <!--<input  type = "hidden"  name = "_token"  value = "{{{csrf_token ()}}}"  />-->
-           <div class="login-form">
-               {{-- Preguntamos si hay alg�n mensaje de error y si hay lo mostramos  --}}
-                    @if(Session::has('mensaje'))
-                    <div class="alert alert-info alert-dismissable">
-                       <button type="button" class="close" data-dismiss="alert">&times;</button>
-                      {{ Session::get('mensaje') }}
-                      </div>
-                     @endif
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Inicie sesión</h3>
+        </div>
+        <div class="panel-body">
+            {!! Form::open(['url'=>'login']) !!}
+            <!--<input  type = "hidden"  name = "_token"  value = "{{{csrf_token ()}}}"  />-->
+                <div class="login-form">
+                    {{-- Preguntamos si hay alg�n mensaje de error y si hay lo mostramos  --}}
+                         @if(Session::has('mensaje'))
+                         <div class="alert alert-info alert-dismissable">
+                            <button type="button" class="close" data-dismiss="alert">&times;</button>
+                           {{ Session::get('mensaje') }}
+                           </div>
+                          @endif
+                 </div>
+                 <div class="login-form">
+                     <!-- <div class="form-group"> -->
+                    <div class="input-group">
+                       <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                       {!! Form::text('txtnuser', null,array('placeholder' => 'Nombre de usuario', 'class' => 'form-control','autofocus')) !!}
+                    </div>
+                    <div class="input-group">
+                        @if( $errors->has('txtnuser') )
+                        @foreach($errors->get('txtnuser') as $error )
+                         <div class="alert alert-warning alert-dismissable">
+                           <button type="button" class="close" data-dismiss="alert">&times;</button>
+                             Ingrese su usuario
+                          </div>
+                        @endforeach
+                        @endif
+                    </div>
+                     <br>
+                     <!-- <div class="form-group"> -->
+                     <div class="input-group">
+                       <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                       {!! Form::password('txtpassword', array('placeholder' => 'Contraseña','class' => 'form-control')) !!}
+                     </div>
+                     <div class="input-group">
+                         @if( $errors->has('txtpassword') )
+                         @foreach($errors->get('txtpassword') as $error )
+                         <div class="alert alert-warning alert-dismissable">
+                           <button type="button" class="close" data-dismiss="alert">&times;</button>
+                              La contraseña debe contener almenos 8 digitos
+                           </div>
+                         @endforeach
+                         @endif
+                     </div>
+
+                     <br>
+                     <div class="input-group">
+                         <p>
+                             <a href="{{ URL::to('/password_recovery') }}">¡He olvidado mi contraseña!</a>
+                         </p>
+                     </div>
+                     <br>
+
+                     <div>
+                       <div class="btn-group">
+                         {!! Form::submit('Iniciar sesión',array('class'=>'btn btn-default boton'))!!}
+                       </div>
+                     </div>
+
+                 </div>
+            {!! Form::close() !!}
             </div>
-              <div class="login-form">
-                <!-- <div class="form-group"> -->
-                <div class="input-group">
-                  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                  {!! Form::text('txtnuser', null,array('placeholder' => 'Nombre de usuario', 'class' => 'form-control','autofocus')) !!}
-                  @if( $errors->has('txtnuser') )
-                  @foreach($errors->get('txtnuser') as $error )
-                   <div class="alert alert-warning alert-dismissable">
-                     <button type="button" class="close" data-dismiss="alert">&times;</button>
-                       Ingrese su usuario
-                    </div>
-                  @endforeach
-                  @endif
-                </div>
-                <br>
-                <!-- <div class="form-group"> -->
-                <div class="input-group">
-                  <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                  {!! Form::password('txtpassword', array('placeholder' => 'Contraseña','class' => 'form-control')) !!}
-                  @if( $errors->has('txtpassword') )
-                  @foreach($errors->get('txtpassword') as $error )
-                  <div class="alert alert-warning alert-dismissable">
-                    <button type="button" class="close" data-dismiss="alert">&times;</button>
-                       La contraseña debe contener almenos 8 digitos
-                    </div>
-                  @endforeach
-                  @endif
-                </div>
-                <br>
-                <div>
-                  <div class="btn-group">
-                    {!! Form::submit('Iniciar sesión',array('class'=>'btn btn-default boton'))!!}
-                  </div>
-                </div>
-                {!! Form::close() !!}
-          </div>
+        </div>
+    </div>
 </div>
 @stop
